ba3b2e391144caf012f7c67c4b0cee387bcb2360
nadiamohamdian <nadia2002mh@gmail.com>
2026-05-31 13:10:31 +0330

    chore: modernize husky hooks

--- .husky/install.mjs
+++
@@ -1,6 +0,0 @@
-import { execSync } from 'child_process';
-try {
-  execSync('pnpm --silent exec husky install', { stdio: 'inherit' });
-} catch (e) {
-  // noop during CI or when husky isn't available locally
-}
